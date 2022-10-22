d63d9930a60bf82a5614a2b2bc1c3df608ca4bae
WYMao <62085936+namaws@users.noreply.github.com>
2022-10-22 01:42:04 -0400

    Time: 2 ms (93.09%), Space: 44.4 MB (86.29%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new LinkedList<>();
+        LinkedList<Integer> curr = new LinkedList<>();
+        curr.add(0);
+        helper(graph, result, curr, 0);
+        return result;
+    }
+    
+    
+    public void helper(int[][] graph, List<List<Integer>> result, LinkedList<Integer> curr, int node) {
+        //reach the end node
+        if(node == graph.length-1) {
+            result.add(new ArrayList(curr));
+        }
+        for(int i=0; i<graph[node].length; i++) {
+            int nextNode = graph[node][i];
+            curr.add(nextNode);
+            helper(graph, result, curr, nextNode);
+            curr.removeLast();
+        }
+    }
+}
