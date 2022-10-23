70805b7211ac944e972b808f84e8f82e384121f8
WYMao <62085936+namaws@users.noreply.github.com>
2022-10-22 23:19:52 -0400

    Time: 8 ms (39.78%), Space: 57.3 MB (53.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,28 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
-        int rLen = grid.length;
-        int cLen = grid[0].length;
-        int numIsland = 0;
-        for(int r = 0; r<rLen; r++) {
-            for(int c=0; c<cLen; c++) {
-                if(grid[r][c] == '1') {
-                    numIsland ++;
+        int row = grid.length;
+        int col = grid[0].length;
+        int count = 0;
+        
+        for(int r=0; r<row; r++) {
+            for(int c=0; c<col; c++) {
+                if(grid[r][c]=='1') {
+                    count++;
                     dfs(grid, r, c);
                 }
             }
         }
-        return numIsland;
+        return count;
     }
     
     public void dfs(char[][] grid, int r, int c) {
-        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c] == '0') {
+        int row = grid.length;
+        int col = grid[0].length;
+        
+        if(r < 0 || r >=row || c < 0 || c >= col || grid[r][c] == '0') {
             return;
         }
         grid[r][c] = '0';
-        
         dfs(grid, r, c+1);
         dfs(grid, r, c-1);
         dfs(grid, r+1, c);
