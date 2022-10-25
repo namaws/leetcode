d49e89e5998bcb415152af225fddb784d6d2c015
WYMao <62085936+namaws@users.noreply.github.com>
2022-10-25 12:19:21 -0400

    Time: 14 ms (81.65%), Space: 54.5 MB (46.62%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -10,8 +10,7 @@ class Solution {
             --count[c2-'a'];
         }
         int cnt = 0;
-        // here only calculate one side
-        // if count tArr[i] - sArr[i], would count the twice of difference
+
         for (int step : count) {
             if (step > 0) {
                 cnt += step;
