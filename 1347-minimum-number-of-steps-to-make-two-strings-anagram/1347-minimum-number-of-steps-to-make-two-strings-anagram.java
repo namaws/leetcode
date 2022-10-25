d36fa83af7d004553aea6e57ea396a840027b977
WYMao <62085936+namaws@users.noreply.github.com>
2022-10-25 12:19:11 -0400

    Time: 31 ms (31.15%), Space: 54.1 MB (66.86%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -1,24 +1,23 @@
 class Solution {
     public int minSteps(String s, String t) {
         // character 
-        int[] sArr = new int[26];
-        int[] tArr = new int[26];
+        int[] count = new int[26];
         
         for(int i=0; i<s.length(); i++) {
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-            sArr[c1-'a'] += 1;
-            tArr[c2-'a'] += 1;
+            ++count[c1-'a'];
+            --count[c2-'a'];
         }
         int cnt = 0;
         // here only calculate one side
         // if count tArr[i] - sArr[i], would count the twice of difference
-        for(int i=0; i<26; i++) {
-            if(sArr[i] > tArr[i]) {
-                cnt += sArr[i]-tArr[i];
+        for (int step : count) {
+            if (step > 0) {
+                cnt += step;
             }
         }
         
-        return Math.abs(cnt);
+        return cnt;
     }
 }
