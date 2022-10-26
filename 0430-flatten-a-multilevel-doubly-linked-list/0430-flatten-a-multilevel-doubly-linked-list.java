6d5d70add0cbbea38e3d6b004f5e840d9aff35bc
WYMao <62085936+namaws@users.noreply.github.com>
2022-10-26 18:20:01 -0400

    Time: 0 ms (100.00%), Space: 42.6 MB (19.19%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -9,27 +9,39 @@ class Node {
 */
 
 class Solution {
+    /*
+    1. Start form the head , move one step each time to the next node
+    2. When meet with a node with child, say node p, follow its child chain to the end and connect the tail node with       p.next, by doing this we merged the child chain back to the main thread
+    3. Return to p and proceed until find next node with child.
+    4. Repeat until reach null
+    */
     public Node flatten(Node head) {
-        if(head == null) return head;
-        // make sure the 'prev' pointer is never gone
-        Node dummy = new Node(0, null, head, null);
-        
-        dfs(dummy, head);
-        dummy.next.prev = null;
-        return dummy.next;
-        
-    }
-    
-    public Node dfs(Node prev, Node curr) {
-       if(curr == null) return prev;
-        curr.prev = prev;
-        prev.next = curr;
-        
-        Node tempNext = curr.next;
-        Node tail = dfs(curr, curr.child);
-        curr.child = null;
-        
-        return dfs(tail, tempNext);
+        if(head == null) return null;
+        Node node = head;
+        while(node != null) {
+            //case 1. No child node, move to next node
+            if(node.child == null) {
+                node = node.next;
+                continue;
+            }
+            
+            //case 2. has Child node
+            Node temp = node.child;
+            
+            //find the tail of the child
+            while(temp.next != null) {
+                temp = temp.next;
+            }
+            // connect tail to main level
+            temp.next = node.next;
+            if(node.next != null)
+                node.next.prev = temp;
+            
+            node.next = node.child;
+            node.child.prev = node;
+            node.child = null;
+        }
+        return head;
     }
 
 }
