4a81bbe8182c48fc1c8db8dc3dfb7a6f4f5ce651
WYMao <62085936+namaws@users.noreply.github.com>
2022-10-25 21:27:33 -0400

    Time: 14 ms (81.65%), Space: 54.5 MB (46.62%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.java
@@ -6,15 +6,17 @@ class Solution {
         for(int i=0; i<s.length(); i++) {
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
-            count[c1-'a']++;
-            count[c2-'a']--;
+            ++count[c1-'a'];
+            --count[c2-'a'];
         }
         int cnt = 0;
-        for (int i=0; i<26; i++) {
-            if (count[i] > 0) {
-                cnt += count[i];
+
+        for (int step : count) {
+            if (step > 0) {
+                cnt += step;
             }
         }
+        
         return cnt;
     }
 }
