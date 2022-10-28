65a9ac11a06e1ad71a24c6a3c0463284afed0433
WYMao <62085936+namaws@users.noreply.github.com>
2022-10-27 23:34:18 -0400

    Time: 11 ms (71.04%), Space: 45.1 MB (58.20%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,32 +1,24 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        /*
-        1. sorting based on the start time of each meeting -> custom comparator O(N*logN)
-        2. [s1,e1] [s2,e2]
-                i.   j
-            if(i>j) -> try to find a room for meeting2
-            else -> don't need to do anything(cuz it can just use the meeting room after meeting 1 finish)
-        
-        3. how to find a meeting room?
-            using minHeap???
-        */
-        int[] startTime = new int[intervals.length];
-        int[] endTime = new int[intervals.length];
-        
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
         for(int i=0; i<intervals.length; i++) {
-            startTime[i] = intervals[i][0];
-            endTime[i] = intervals[i][1];
-        }        
-        Arrays.sort(startTime);
-        Arrays.sort(endTime);
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        
+        Arrays.sort(start);
+        Arrays.sort(end);
         int room = 0;
-        int endIdx = 0;
-        for(int i=0; i<startTime.length; i++) {
-            if(startTime[i] < endTime[endIdx]) {
-                room ++;
+        int currIdx = 0;
+        for(int i=0; i<intervals.length; i++) {
+            
+            //overlap
+            if(start[i] < end[currIdx]) {
+                room += 1;
             }
             else {
-                endIdx++;
+                currIdx ++;
             }
         }
         return room;
