a434d00b243da648c53c8d02a07f55de8db4ab4c
WYMao <62085936+namaws@users.noreply.github.com>
2022-10-28 10:15:59 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (82.59%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -9,39 +9,31 @@ class Node {
 */
 
 class Solution {
-    /*
-    1. Start form the head , move one step each time to the next node
-    2. When meet with a node with child, say node p, follow its child chain to the end and connect the tail node with       p.next, by doing this we merged the child chain back to the main thread
-    3. Return to p and proceed until find next node with child.
-    4. Repeat until reach null
-    */
     public Node flatten(Node head) {
-        if(head == null) return null;
-        Node node = head;
-        while(node != null) {
-            //case 1. No child node, move to next node
-            if(node.child == null) {
-                node = node.next;
-                continue;
-            }
-            
-            //case 2. has Child node
-            Node temp = node.child;
-            
-            //find the tail of the child
-            while(temp.next != null) {
-                temp = temp.next;
-            }
-            // connect tail to main level
-            temp.next = node.next;
-            if(node.next != null)
-                node.next.prev = temp;
-            
-            node.next = node.child;
-            node.child.prev = node;
-            node.child = null;
-        }
-        return head;
+        if(head == null) return head;
+        
+        Node dummy = new Node(0, null, head, null);
+        
+        //each dfs flatten one level
+        flattenDFS(dummy, head);
+        
+        // head's prev pointer should be null
+        dummy.next.prev = null;
+        return dummy.next;
     }
-
+    
+    public Node flattenDFS(Node prev, Node curr) {
+        if(curr == null) return prev;
+        curr.prev = prev;
+        prev.next = curr;
+        
+        Node temp = curr.next;
+        //cuz need to see if curr has child
+        // yes -> flatten 
+        // no -> return itself
+        Node tail = flattenDFS(curr, curr.child);
+        curr.child = null;
+        
+        return flattenDFS(tail, temp);
+    }
 }
