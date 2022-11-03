fd8e978db2264beae65fb8ab518cbe9d236ce81e
WYMao <62085936+namaws@users.noreply.github.com>
2022-11-03 00:14:38 -0400

    Time: 1 ms (64.65%), Space: 44.6 MB (10.68%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,38 +14,21 @@
  * }
  */
 class Solution {
-    Deque<TreeNode> stack = new LinkedList();
-    Deque<Integer> upper = new LinkedList();
-    Deque<Integer> lower = new LinkedList();
     
     public boolean isValidBST(TreeNode root) {
-        if(root == null) return true;
-
-        Integer low = null, high = null, val;
-        update(root, low, high);
-        
-        while(!stack.isEmpty()) {
-            root = stack.poll();
-            low = lower.poll();
-            high = upper.poll();
-            
-            if(root == null) continue;
-            val = root.val;
-            if(low != null && val <= low) {
-                return false;
-            }
-            if(high != null && val >= high) {
-                return false;
-            }
-            update(root.right,val,high);
-            update(root.left,low,val);
-        }
-        return true;
+        return isValid(root, null, null);
     }
     
-    public void update(TreeNode node, Integer low, Integer high) {
-        stack.add(node);
-        lower.add(low);
-        upper.add(high);
+    public boolean isValid(TreeNode node, Integer low, Integer high) {
+        if(node == null) return true;
+        if(low != null && node.val <= low) {
+            return false;
+        }
+        if(high != null && node.val >= high) {
+            return false;
+        }
+        
+        return isValid(node.left, low, node.val) && isValid(node.right, node.val, high);
     }
+   
 }
