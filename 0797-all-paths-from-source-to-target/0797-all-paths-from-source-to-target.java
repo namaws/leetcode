b570c2783d0f8c9e9c7c28714365d40ce07e1a3e
WYMao <62085936+namaws@users.noreply.github.com>
2022-11-03 20:59:26 -0400

    Time: 7 ms (35.87%), Space: 55.2 MB (45.77%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,5 +1,10 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        /*
+        Using recursive function to find the path
+        
+        */
+        
         List<List<Integer>> result = new LinkedList<>();
         LinkedList<Integer> curr = new LinkedList<>();
         curr.add(0);
@@ -7,12 +12,12 @@ class Solution {
         return result;
     }
     
-    
     public void helper(int[][] graph, List<List<Integer>> result, LinkedList<Integer> curr, int node) {
-        //reach the end node
+        //base case -> reach the end node -> find the possible path to the end
         if(node == graph.length-1) {
             result.add(new ArrayList(curr));
         }
+        
         for(int i=0; i<graph[node].length; i++) {
             int nextNode = graph[node][i];
             curr.add(nextNode);
