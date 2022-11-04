3c428e47779e8a231c95aea2332f3b55a8810d2d
WYMao <62085936+namaws@users.noreply.github.com>
2022-11-04 10:44:23 -0400

    Time: 26 ms (98.52%), Space: 92.8 MB (84.18%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,37 +1,37 @@
 class RandomizedSet {
-    //hashmap to store the value and the inde
-    Map<Integer, Integer> map = new HashMap<>();
-    List<Integer> list = new ArrayList<>();
-    java.util.Random rand = new java.util.Random();
-    public RandomizedSet() {}
+    private List<Integer> list;
+    private Map<Integer, Integer> map;
+    private Random rand = new Random();
+    
+    public RandomizedSet() {
+        list = new ArrayList();
+        map = new HashMap();
+    }
     
     public boolean insert(int val) {
-        if(list != null && map.containsKey(val)) {
+        if(map.containsKey(val)) {
             return false;
-        } else {
+        }
+        else {
             list.add(val);
             map.put(val, list.size()-1);
             return true;
         }
-        
     }
     
     public boolean remove(int val) {
-        // if the val is in the last position
         if(map.containsKey(val)) {
             int idx = map.get(val);
             if(idx < list.size()-1) {
                 int last = list.get(list.size()-1);
                 map.put(last, idx);
-                list.set(idx, last);
+                list.set(idx,last);
             }
             map.remove(val);
             list.remove(list.size()-1);
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     }
     
     public int getRandom() {
