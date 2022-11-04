ff4cc0c1d4075c15d3a3262fe9955f3fc14e2495
WYMao <62085936+namaws@users.noreply.github.com>
2022-11-04 11:55:10 -0400

    Time: 2 ms (100.00%), Space: 41.8 MB (98.31%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -2,6 +2,7 @@ class Solution {
     public int minMeetingRooms(int[][] intervals) {
         int[] start = new int[intervals.length];
         int[] end = new int[intervals.length];
+        
         for(int i=0; i<intervals.length; i++) {
             start[i] = intervals[i][0];
             end[i] = intervals[i][1];
@@ -10,15 +11,13 @@ class Solution {
         Arrays.sort(start);
         Arrays.sort(end);
         int room = 0;
-        int currIdx = 0;
-        for(int i=0; i<intervals.length; i++) {
-            
-            //overlap
-            if(start[i] < end[currIdx]) {
-                room += 1;
+        int endIdx = 0;
+        for(int i=0; i<start.length; i++) {
+            if(start[i] < end[endIdx]) {
+                room ++;
             }
             else {
-                currIdx ++;
+                endIdx ++;
             }
         }
         return room;
