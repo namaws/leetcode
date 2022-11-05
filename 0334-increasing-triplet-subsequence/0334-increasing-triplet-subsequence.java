038ea1bfef2492e2aecd62717f1607df0233b585
WYMao <62085936+namaws@users.noreply.github.com>
2022-11-04 20:21:20 -0400

    Time: 18 ms (6.12%), Space: 93.1 MB (67.93%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        ArrayList<Integer> sub = new ArrayList();
+        sub.add(nums[0]);
+        
+        for(int i=1; i<nums.length; i++) {
+            int num = nums[i];
+            if(num > sub.get(sub.size()-1)) {
+                sub.add(num);
+            }
+            else {
+                int j = binarySearch(sub, num);
+                sub.set(j, num);
+            }
+            if(sub.size() == 3) return true;
+        }
+        return false;
+    }
+    
+    private int binarySearch(ArrayList<Integer> sub, int num) {
+        int left = 0;
+        int right = sub.size()-1;
+        
+        while(left < right) {
+            int mid = (left+right)/2;
+            if(sub.get(mid) == num) return mid;
+            if(sub.get(mid) < num)
+                left = mid+1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
