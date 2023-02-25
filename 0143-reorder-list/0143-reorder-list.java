ffb15c0170ebc0f8543c005eaabcb4a46b7068ae
WYMao <2maomao54@gmail.com>
2023-02-25 16:17:50 -0500

    Time: 1 ms (100.00%), Space: 44.9 MB (75.29%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,31 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
     public void reorderList(ListNode head) {
         ListNode slow = head;
         ListNode fast = head.next;
-        while (fast != null && fast.next != null) {
+        
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
-        ListNode second = slow.next;
-        ListNode prev = slow.next = null;
-        while (second != null) {
-            ListNode tmp = second.next;
-            second.next = prev;
-            prev = second;
-            second = tmp;
+        
+        ListNode middle = slow.next;
+        ListNode preMiddle = slow.next = null;
+        while(middle != null) {
+            ListNode temp = middle.next;
+            middle.next = preMiddle;
+            preMiddle = middle;
+            middle = temp;
         }
-
-        ListNode first = head;
-        second = prev;
-        while (second != null) {
-            ListNode tmp1 = first.next;
-            ListNode tmp2 = second.next;
-            first.next = second;
-            second.next = tmp1;
-            first = tmp1;
-            second = tmp2;
+        
+        ListNode pointer = head;
+        while(preMiddle != null) {
+            ListNode tmp1 = pointer.next;
+            ListNode tmp2 = preMiddle.next;
+            pointer.next = preMiddle;
+            preMiddle.next = tmp1;
+            preMiddle = tmp2;
+            pointer = tmp1;
         }
     }
 }
