d1e096e1a08f2ea8e473fd34eb0916d58deaa2a2
WYMao <2maomao54@gmail.com>
2023-03-05 22:05:28 -0500

    Time: 12 ms (56.64%), Space: 56.1 MB (44.67%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet();
+        for(int i: nums) {
+            if(!set.contains(i)) {
+                set.add(i);
+            }
+            else return true;
+        }
+        return false;
+    }
+}
