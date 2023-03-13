25c61176c645c9062504e075510c468f989f5488
WYMao <2maomao54@gmail.com>
2023-03-13 15:34:02 -0400

    Time: 13 ms (31.98%), Space: 43.8 MB (17.26%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -3,23 +3,27 @@ public class Codec {
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
-        for(int i=0; i<strs.size(); i++) {
-            sb.append(strs.get(i).length()+"#"+strs.get(i));
+        for(String str: strs) {
+            sb.append(str.length()+"#"+str);
         }
         return sb.toString();
-        
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> list = new ArrayList();
-        int i=0;
-        while(i<s.length()) {
-            int j=i;
-            while(s.charAt(j)!='#')j++;
-            int length = Integer.valueOf(s.substring(i,j));
-            i = j+1+length;
-            list.add(s.substring(j+1, i));
+        int p1 = 0;
+        while(p1 < s.length()) {
+            int p2 = p1;
+            //find the number of length
+            while(s.charAt(p2) != '#') {
+                p2++;
+            }
+            // change the string of number into integer
+            int length = Integer.valueOf(s.substring(p1, p2));
+            //make p1 go all the way to the end of that word
+            p1 = p2 + length + 1;
+            list.add(s.substring(p2+1, p1));
         }
         return list;
     }
