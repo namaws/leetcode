e2e8d4bd73e140527e5109f2cfdb457fc277865f
WYMao <2maomao54@gmail.com>
2023-03-19 23:52:08 -0400

    Time: 2342 ms (80.48%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -3,5 +3,5 @@ select s.name from salesperson s
 where s.sales_id not in (
     select o.sales_id from orders o
     left join company c on o.com_id = c.com_id
-    where c.name = 'RED'
+    where c.name like 'RED'
 )
