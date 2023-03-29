f83eaf78a74223773bbe72156930dc3df82e3478
WYMao <2maomao54@gmail.com>
2023-03-29 00:05:06 -0400

    Time: 1 ms (98.99%), Space: 45.3 MB (48.54%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,9 +3,12 @@ class Solution {
         int left = 0;
         int right = numbers.length-1;
         while(left <= right) {
-            if(numbers[left] + numbers[right] == target) break;
-            if(numbers[left] + numbers[right] > target) right--;
-            else left++;
+            if(numbers[left]+numbers[right] == target)
+                break;
+            else if(numbers[left]+numbers[right] < target)
+                left++;
+            else
+                right--;
         }
         return new int[]{left+1, right+1};
     }
