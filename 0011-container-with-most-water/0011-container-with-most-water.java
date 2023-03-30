fd560d2a9bb8fab97378ba50a64b206d1d1a3085
WYMao <2maomao54@gmail.com>
2023-03-29 20:23:14 -0400

    Time: 4 ms (67.53%), Space: 52.7 MB (44.33%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,18 +3,12 @@ class Solution {
         int left = 0, right = height.length-1;
         int container = 0;
         while(left <= right) {
-            int curr = Math.min(height[left], height[right])*(right-left);
-            container = Math.max(curr, container);
-            //can potential find bigger height if move right (since height[left] already bigger than height[right] at the current moment)
+            //current water container volumn
+            int currVol = Math.min(height[left], height[right])*(right-left);
+            container = Math.max(currVol, container);
             if(height[left] >= height[right]) right--;
             else left++;
         }
         return container;
     }
 }
-
-/**
-[1,8,6,2,5,4,8,3,7]
- ^               ^ (height.length-1)
-              
-*/
