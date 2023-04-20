ed1a4062e99ffc97f5cf73f17f236a01bdbf4fbd
WYMao <2maomao54@gmail.com>
2023-04-20 13:28:44 -0400

    Time: 2 ms (100.00%), Space: 50.9 MB (45.31%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,19 +14,16 @@
  * }
  */
 class Solution {
-
     public int goodNodes(TreeNode root) {
-        return helper(root, -99999);
+        return helper(root, Integer.MIN_VALUE);
     }
-
     public int helper(TreeNode root, int max) {
-        if (root == null) return 0;
-
-        int res = root.val >= max ? 1 : 0;
-
+        if(root == null) return 0;
+        
+        int res = root.val >= max? 1:0;
+        
         res += helper(root.left, Math.max(root.val, max));
         res += helper(root.right, Math.max(root.val, max));
-
         return res;
     }
 }
