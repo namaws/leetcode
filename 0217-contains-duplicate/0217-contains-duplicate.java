ce4f169060b577ab1a8ab0ecda0eeea6a0f2f758
WYMao <2maomao54@gmail.com>
2023-06-18 19:53:20 -0400

    Time: 14 ms (40.89%), Space: 55.2 MB (76.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,6 +2,9 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet();
         for(int num: nums) {
+            //set.add(num) will return boolean value
+            //true -> num can add in the set, meaning num is not in the set yet
+            //false -> num cannot be added in the set, meaning num is already existed
             if(!set.add(num))
                 return true;
             set.add(num);
