83f0ca05bb710cbb8b8332c1c811916733f75797
WYMao <2maomao54@gmail.com>
2023-06-29 17:06:06 -0400

    Time: 3 ms (71.69%), Space: 43.6 MB (35.30%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,30 +1,21 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        List<Set<Character>> subBox = new ArrayList();
-        List<Set<Character>> row = new ArrayList();
-        List<Set<Character>> col = new ArrayList();
+        //create list for rol, col and box
+        //the element in the list is hashset, no dulplicate allow in the set
+        List<Set<Character>> rowCnt = new ArrayList();
+        List<Set<Character>> colCnt = new ArrayList();
+        List<Set<Character>> boxCnt = new ArrayList();
         for(int i=0; i<9; i++) {
-            subBox.add(new HashSet());
-            row.add(new HashSet());
-            col.add(new HashSet());
+            rowCnt.add(new HashSet());
+            colCnt.add(new HashSet());
+            boxCnt.add(new HashSet());
         }
-        
-        /**
-        subBox
-        0, 1, 2
-        3, 4, 5
-        6, 7, 8
-        */
         for(int r=0; r<9; r++) {
             for(int c=0; c<9; c++) {
-                char ch = board[r][c];
-                if(ch == '.') continue;
-                else if(row.get(r).contains(ch) || col.get(c).contains(ch) || subBox.get(r/3*3+c/3).contains(ch)) {
-                    return false;
-                }
-                row.get(r).add(ch);
-                col.get(c).add(ch);
-                subBox.get(r/3*3+c/3).add(ch);
+                if(board[r][c] == '.') continue;
+                if(!rowCnt.get(r).add(board[r][c])) return false;
+                if(!colCnt.get(c).add(board[r][c])) return false;
+                if(!boxCnt.get(r/3*3+c/3).add(board[r][c])) return false;
             }
         }
         return true;
