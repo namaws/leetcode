47d363d3635db131079668b1258e9f683b6b4c83
WYMao <2maomao54@gmail.com>
2023-07-01 22:39:48 -0400

    Time: 6 ms (98.49%), Space: 45.8 MB (61.78%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,29 +1,26 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
+        int len = mat.length + mat[0].length;
         int row = mat.length;
         int col = mat[0].length;
-        int INF = row+col;
-        //first examine top and left of each cell
-        // -> don't need to examine when row/col = 0
+        //top, left
         for(int r=0; r<row; r++) {
             for(int c=0; c<col; c++) {
+                int left = len, top = len;
                 if(mat[r][c] == 0) continue;
-                int left = INF, top = INF;
                 if(r-1 >= 0) top = mat[r-1][c];
                 if(c-1 >= 0) left = mat[r][c-1];
-                mat[r][c] = Math.min(left, top)+1;
+                mat[r][c] = Math.min(top,left)+1;
             }
         }
-        
+        //down, right
         for(int r=row-1; r>=0; r--) {
             for(int c=col-1; c>=0; c--) {
+                int right = len, down = len;
                 if(mat[r][c] == 0) continue;
-                int right = INF, down = INF;
                 if(r+1 < row) down = mat[r+1][c];
                 if(c+1 < col) right = mat[r][c+1];
-                //need to double check mat itself, due to if the cell is the right down corner
-                //it would not go to the above if statement, the right and down would stay INF
-                mat[r][c] = Math.min(mat[r][c],Math.min(right, down)+1);
+                mat[r][c] = Math.min(mat[r][c], Math.min(down, right)+1);
             }
         }
         return mat;
