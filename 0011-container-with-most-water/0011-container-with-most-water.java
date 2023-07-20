d820bda2c3c96f293f3cf689e789ab21940e58cb
WYMao <2maomao54@gmail.com>
2023-07-20 18:06:16 -0400

    Time: 4 ms (83.08%), Space: 55.9 MB (15.58%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -2,11 +2,11 @@ class Solution {
     public int maxArea(int[] height) {
         int left = 0, right = height.length-1;
         int container = 0;
-        while(left <= right) {
-            //current water container volumn
-            int currVol = Math.min(height[left], height[right])*(right-left);
-            container = Math.max(currVol, container);
-            if(height[left] >= height[right]) right--;
+        while(left < right) {
+            int currVol = Math.min(height[right], height[left])*(right-left);
+            container = Math.max(container, currVol);
+            //move the smaller value of pointer, as trying to find the max
+            if(height[left]>=height[right]) right--;
             else left++;
         }
         return container;
