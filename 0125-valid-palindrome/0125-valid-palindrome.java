942f34908a7431f0ac1e59b4ee23b83ef0987018
Mao <2maomao54@gmail.com>
2024-06-04 22:14:22 -0500

    Time: 3 ms (73.08%), Space: 42.6 MB (91.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        String str = s.toLowerCase();
+        
         if(s.length() == 0 || s.length() == 1) return true;
-        int left = 0;
-        int right = s.length()-1;
+        
+        int left = 0, right = s.length()-1;
         while(left <= right) {
             if(!Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
                 continue;
             }
+            
             if(!Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
                 continue;
@@ -15,12 +18,13 @@ class Solution {
             
             char cLeft = Character.toLowerCase(s.charAt(left));
             char cRight = Character.toLowerCase(s.charAt(right));
-            if(cLeft != cRight)
-                return false;
-            else {
+            if(cLeft == cRight) {
                 left++;
                 right--;
             }
+            else
+                return false;
+            
         }
         return true;
     }
