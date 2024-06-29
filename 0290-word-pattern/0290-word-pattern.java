f0e621cb7679266b444c96cfe4c5cec2ebb5ff58
Mao <2maomao54@gmail.com>
2024-06-29 12:54:17 -0500

    Time: 2 ms (12.97%), Space: 41.5 MB (20.98%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,30 +1,31 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-//        Map<Character, List<Integer>> pMap = new HashMap();
-//        Map<String, List<Integer>> sMap = new HashMap();
-        //split s into String array
-        String[] strs = s.split("\\s+");
-        char[] patterns = pattern.toCharArray();
         
-        if(strs.length != patterns.length) return false;
+    
+        //Mapping for pattern chracter and string
         Map<Character, String> map = new HashMap();
         
+        //loop through pattern
+        //1. if both non-exist -> new mapping
+        //2. if key not exist but value exist -> return false
+        //3. if key exist -> check if value is the same
+        
+        String[] strs = s.split("\\s+");
+        char[] charArr = pattern.toCharArray();
+        
+        if(strs.length != charArr.length) return false;
         for(int i=0; i<strs.length; i++) {
-            if(!map.containsKey(patterns[i])) {
-                //already have matching
+            if(!map.containsKey(charArr[i])) {
                 if(map.containsValue(strs[i]))
                     return false;
-                
-                map.put(patterns[i], strs[i]);
+                else
+                    map.put(charArr[i], strs[i]);
             }
-            
             else {
-                if(!map.get(patterns[i]).equals(strs[i]))
+                if(!map.get(charArr[i]).equals(strs[i]))
                     return false;
             }
-            
         }
         return true;
-        
     }
 }
