692978c6ea2d83827a4adc4f175285c50e064fe9
Mao <2maomao54@gmail.com>
2024-06-28 23:42:06 -0500

    Time: 2 ms (12.97%), Space: 41.3 MB (50.73%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -1,32 +1,30 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
+//        Map<Character, List<Integer>> pMap = new HashMap();
+//        Map<String, List<Integer>> sMap = new HashMap();
+        //split s into String array
         String[] strs = s.split("\\s+");
-        if(pattern.length() != strs.length) return false;
-        Map<String, Integer> sMap = new HashMap();
-        Map<Character, Integer> pMap = new HashMap();
         char[] patterns = pattern.toCharArray();
-        int sCnt = 0;
-        int pCnt = 0;
-        int[] sArr = new int[strs.length];
-        int[] pArr = new int[strs.length];
-        for(int i=0; i<patterns.length; i++) {
-            if(!pMap.containsKey(patterns[i])) {
-                pCnt += 1;
-                pMap.put(patterns[i], pCnt);
-            }
-            pArr[i] = pMap.get(patterns[i]);
-            if(!sMap.containsKey(strs[i])) {
-                sCnt += 1;
-                sMap.put(strs[i], sCnt);
-            }
-            sArr[i] = sMap.get(strs[i]);
-        }
         
-        for(int i=0; i<pArr.length; i++) {
-            if(pArr[i] != sArr[i])
-                return false;
+        if(strs.length != patterns.length) return false;
+        Map<Character, String> map = new HashMap();
+        
+        for(int i=0; i<strs.length; i++) {
+            if(!map.containsKey(patterns[i])) {
+                //already have matching
+                if(map.containsValue(strs[i]))
+                    return false;
+                
+                map.put(patterns[i], strs[i]);
+            }
+            
+            else {
+                if(!map.get(patterns[i]).equals(strs[i]))
+                    return false;
+            }
+            
         }
         return true;
+        
     }
-    
 }
