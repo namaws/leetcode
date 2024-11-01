0627a79542a32d97c36a5f5846570a3f90eb4a6a
Mao <2maomao54@gmail.com>
2024-10-31 22:34:22 -0500

    Time: 1 ms (100.00%), Space: 42.4 MB (51.68%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int myAtoi(String s) {
+        int len = s.length();
+        if(len == 0) return 0;
+        
+        //step 1. remove space
+        int idx = 0;
+        while(idx < len && s.charAt(idx) == ' ') {
+            idx++;
+        }
+        
+        if(idx == len) return 0;
+        //step 2. check sign
+        boolean isNeg = false;
+        if(s.charAt(idx) == '+' || s.charAt(idx) == '-') {
+            isNeg = s.charAt(idx++) == '-'? true:false;
+        }
+        
+        int total = 0;
+        //step 3. conversion
+        while(idx < len) {
+            int digit = s.charAt(idx)-'0';
+            if(digit < 0 || digit > 9) break;
+            
+            //if need to round up
+            if(Integer.MAX_VALUE/10 < total || (Integer.MAX_VALUE/10 == total && Integer.MAX_VALUE%10 < digit))
+                return isNeg == true? Integer.MIN_VALUE: Integer.MAX_VALUE;
+            total = 10*total+digit;
+            idx++;   
+        }
+        
+        return isNeg == true? total*-1:total;
+    }
+}
