aee629ca2efc1d73ebe8d16ee0aac288603881ad
Mao <2maomao54@gmail.com>
2025-05-01 19:17:39 -0500

    Time: 0 ms (100%), Space: 42.9 MB (46.81%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,22 +1,24 @@
 class Solution {
-    private Set<List<Integer>> set = new HashSet();
+    private List<List<Integer>> result = new ArrayList();
 
     public List<List<Integer>> subsets(int[] nums) {
-        backtrack(0, new ArrayList(), nums, set);
+        backtrack(0, new ArrayList(), nums);
+        /*
         List<List<Integer>> result = new ArrayList();
         for(List<Integer> list: set) {
             result.add(list);
         }
+        */
         return result;
     }
 
-    private void backtrack(int index, List<Integer> curr, int[] nums, Set<List<Integer>> set) {
-        if(!set.contains(curr)) {
-            set.add(new ArrayList(curr));
-        }
+    private void backtrack(int index, List<Integer> curr, int[] nums) {
+        //if(!set.contains(curr)) {
+            result.add(new ArrayList(curr));
+        //}
         for(int i=index; i<nums.length; i++) {
             curr.add(nums[i]);
-            backtrack(i+1, curr, nums, set);
+            backtrack(i+1, curr, nums);
             curr.remove(curr.size()-1);
         }
 
