984f9b07ab08b38618a7e33e24cd9da932cc3f06
Mao <2maomao54@gmail.com>
2025-05-02 13:18:54 -0500

    Time: 7 ms (35.81%), Space: 45.6 MB (92.45%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,44 +1,46 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // the order to take course
+/*
+1. HashMap record the restriction of this course <K,V> -> <prerequisite course, courses restrict by this course>
+2. Restriction Array record the number of restriction of this course
+3. Course Order
+*/      
+        if(numCourses == 0) return null;
+        Map<Integer, List<Integer>> restrictionMap = new HashMap();
+        int[] restrictNums = new int[numCourses];
         int[] order = new int[numCourses];
         int idx = 0;
-        //checking restriction map
-        //<key, value> -> <pre-course, list of courses> => by fulfilling pre-course, the list of courses can be taken
-        Map<Integer, List<Integer>> map = new HashMap();
         
-        if(numCourses == 0) return null;
-        
-        int[] indegree = new int[numCourses];
-        //checking each course restriction, if yes ++ 1
         for(int i=0; i<prerequisites.length; i++) {
             int course = prerequisites[i][0];
-            int preCourse = prerequisites[i][1];
-            map.putIfAbsent(preCourse, new ArrayList());
-            map.get(preCourse).add(course);
-            indegree[course]++; // how many restriction this course have
+            int pre = prerequisites[i][1];
+            restrictionMap.putIfAbsent(pre, new ArrayList());
+            restrictionMap.get(pre).add(course);
+            restrictNums[course]++;
         }
         
-        Queue<Integer> queue = new LinkedList<Integer>();
+        //use queue to check current taken class
+        Queue<Integer> queue = new LinkedList();
+        
+        //add those courses that don't require prerequisites
         for(int i=0; i<numCourses; i++) {
-            //no restriction -> meaning can add course whenever 
-            if(indegree[i] == 0) {
+            if(restrictNums[i] == 0) {
+                order[idx++]=i;
                 queue.offer(i);
-                order[idx++] = i;
             }
         }
         
         while(!queue.isEmpty()) {
-            //already finished course -> check if other course can take because already taken this
-            int prerequisite = queue.poll();
-            if(map.get(prerequisite)!= null) {
-                for(int course: map.get(prerequisite)) {
-                    if(--indegree[course] == 0) {
+            int curr = queue.poll();
+            if(restrictionMap.get(curr) != null) {
+                for(int course: restrictionMap.get(curr)) {
+                    if(--restrictNums[course] == 0) {
                         queue.offer(course);
                         order[idx++] = course;
                     }
                 }
             }
+            
         }
         return idx == numCourses ? order: new int[0];
     }
