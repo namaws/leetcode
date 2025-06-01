40b9eaa44be136d4e24d3a6509276fee071bb01b
Mao <2maomao54@gmail.com>
2025-06-01 13:04:35 -0500

    Time: 3 ms (12.45%), Space: 45.4 MB (6.99%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,9 +13,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         Set<ListNode> set = new HashSet();
         while(head != null) {
-            if(set.contains(head))
-                return true;
-            set.add(head);
+            if(!set.add(head)) return true;
             head = head.next;
         }
         return false;
