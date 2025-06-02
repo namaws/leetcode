6dd662d52f2eefc34e3d3b85229b73afe4e921fe
Mao <2maomao54@gmail.com>
2025-06-01 20:31:22 -0500

    Time: 0 ms (100%), Space: 43.2 MB (55.17%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -13,20 +13,25 @@ class Solution {
         ListNode dummy = new ListNode(-1, head);
         ListNode fast = head;
         ListNode slow = dummy;
-        
+
         while(fast != null) {
-            while(fast.next != null && fast.val == fast.next.val)
+            //while loop for duplicate value 
+            while(fast.next != null && fast.val == fast.next.val) {
                 fast = fast.next;
+            }
+            
+            //remove duplicate
             if(slow.next != fast) {
                 slow.next = fast.next;
                 fast = slow.next;
             }
+
+            //no duplicate -> do normal step
             else {
                 slow = slow.next;
                 fast = fast.next;
             }
         }
-        
         return dummy.next;
     }
 }
