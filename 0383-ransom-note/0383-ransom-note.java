3d60a57e5e222a7dd422dc88b3a3b48871a11e0f
Mao <2maomao54@gmail.com>
2025-06-02 21:15:18 -0500

    Time: 2 ms (82.05%), Space: 44.9 MB (62.2%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,14 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        //magazine dict k,v -> letter, cnt
-        Map<Character, Integer> dict = new HashMap();
+        int[] cnt = new int[26];
         for(int i=0; i<magazine.length(); i++) {
-            char c = magazine.charAt(i);
-            dict.put(c, dict.getOrDefault(c, 0)+1);
+            cnt[magazine.charAt(i)-'a']++;
         }
 
         for(int i=0; i<ransomNote.length(); i++) {
             char c = ransomNote.charAt(i);
-            if(dict.containsKey(c)) {
-                dict.put(c, dict.get(c)-1);//update the number in value
-                if(dict.get(c) == 0) dict.remove(c);//if 0, remove the key value pair
-            }
-            else
-                return false;
+            cnt[c-'a']--;
+            if(cnt[c-'a']<0) return false;
         }
         return true;
     }
