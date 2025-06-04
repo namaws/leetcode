1c4c9f887778fd521cc8495a7129639bf348cca1
Mao <2maomao54@gmail.com>
2025-06-03 21:18:30 -0500

    Time: 1 ms (98.89%), Space: 45.1 MB (26.38%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,33 +1,29 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        
         List<int[]> list = new ArrayList();
-        
-        //3 situation
-        //1. at the very beginning
-        //2. at the very back
-        //3. in the middle
-        
-        int situation = 0;
-        
+        /**
+        1. before current interval
+        2. behind current interval
+        3. overlap
+         */
         for(int[] interval: intervals) {
-            //newInterval is in the front -> no overlap
-            if(newInterval == null || interval[1] < newInterval[0]) {
+            //after current interval
+            if(newInterval == null || interval[1]<newInterval[0])
                 list.add(interval);
-            }
             
-            //newInterval is behind -> don't know if there's an overlap afterwards
+            //before current interval
             else if(interval[0] > newInterval[1]) {
                 list.add(newInterval);
                 list.add(interval);
                 newInterval = null;
             }
+
+            //overlaping
             else {
                 newInterval[0] = Math.min(interval[0], newInterval[0]);
                 newInterval[1] = Math.max(interval[1], newInterval[1]);
             }
         }
-        
         if(newInterval != null) list.add(newInterval);
         return list.toArray(new int[list.size()][]);
     }
