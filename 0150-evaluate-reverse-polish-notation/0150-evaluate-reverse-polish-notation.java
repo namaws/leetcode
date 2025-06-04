b9d931f1539ae6e581a053df2f3d1068164fc6b2
Mao <2maomao54@gmail.com>
2025-06-04 10:21:19 -0500

    Time: 6 ms (72.49%), Space: 44.9 MB (32.23%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,27 +1,30 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        //use stack to give track of current value
         Stack<Integer> stack = new Stack();
-        Set<String> operations = Set.of("+", "-", "*", "/");
-        
         for(int i=0; i<tokens.length; i++) {
-            if(stack.size() >=2 && operations.contains(tokens[i])) {
-                int val2 = stack.pop();
+            if(tokens[i].equals("+") && stack.size() >=2 ) {
                 int val1 = stack.pop();
-                
-                if(tokens[i].equals("+"))
-                    stack.add(val1+val2);
-                else if(tokens[i].equals("-"))
-                    stack.add(val1-val2);
-                else if(tokens[i].equals("*"))
-                    stack.add(val1*val2);
-                else
-                    stack.add(val1/val2);
+                int val2 = stack.pop();
+                stack.push(val1+val2);
+            }
+            else if(tokens[i].equals("-") && stack.size() >= 2) {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+                stack.push(val2-val1);
+            }
+            else if(tokens[i].equals("*") && stack.size() >= 2) {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+                stack.push(val1*val2);
+            }
+            else if(tokens[i].equals("/") && stack.size() >= 2) {
+                int val1 = stack.pop();
+                int val2 = stack.pop();
+                stack.push(val2/val1);
             }
             else
-                stack.add(Integer.valueOf(tokens[i]));
+                stack.push(Integer.valueOf(tokens[i]));
         }
-        
         return stack.pop();
     }
 }
