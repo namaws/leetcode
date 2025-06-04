8779f7dfb61958681f598c4f38d5411b27aec32a
Mao <2maomao54@gmail.com>
2025-06-04 11:29:58 -0500

    Time: 1 ms (100%), Space: 44.8 MB (30.5%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,21 +8,12 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
- /**
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {};
-    ListNode(int val) {this.val = val;}
-    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
-} */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dum = new ListNode(0);
-        ListNode result = dum;
+        ListNode dummy = new ListNode(0);
+        boolean overflow = false;
+        ListNode res = dummy;
         int val = 0;
-        boolean carry = false;
         while(l1 != null || l2 != null) {
             if(l1 == null) {
                 val = l2.val;
@@ -33,28 +24,24 @@ class Solution {
                 l1 = l1.next;
             }
             else {
-                val = l1.val+l2.val;
+                val = l1.val + l2.val;
                 l1 = l1.next;
                 l2 = l2.next;
             }
-            //previous carry over
-            if(carry) val += 1;
 
-            if(val >= 10) {
-                carry = true;
-                val %= 10;
+            if(overflow) {
+                val++;
             }
-            else
-                carry = false;
-            
-            ListNode curr = new ListNode(val);
-            dum.next = curr;
-            dum = dum.next;
+            if(val >= 10) {
+                overflow = true;
+                val%=10;
+            }
+            else overflow = false;
+            dummy.next = new ListNode(val);
+            dummy = dummy.next;
         }
-        if(carry) {
-            ListNode curr = new ListNode(1);
-            dum.next = curr;
-        }
-        return result.next;
+
+        if(overflow) dummy.next = new ListNode(1);
+        return res.next;
     }
 }
