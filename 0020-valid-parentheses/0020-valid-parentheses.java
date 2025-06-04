f3c39d2c192df2cfc8663c931627761b6509c2a9
Mao <2maomao54@gmail.com>
2025-06-04 08:16:57 -0500

    Time: 2 ms (97.29%), Space: 41.7 MB (66.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
+        if(s.length()%2 != 0) return false;
         Stack<Character> stack = new Stack();
-        
+
         for(int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
-            if(c=='(' || c=='{' || c=='[') {
-                stack.add(c);
-            }
+            if(c == '(' || c == '[' || c == '{') 
+                stack.push(c);
             else {
-                if(c == ']' && !stack.isEmpty() && stack.peek() == '[')
-                    stack.pop();
-                else if(c == '}' && !stack.isEmpty() && stack.peek() == '{')
-                    stack.pop();
-                else if(c == ')'&& !stack.isEmpty() && stack.peek() == '(')
-                    stack.pop();
-                else 
-                    return false;
+                if(c == ')' && !stack.empty() && stack.peek() == '(') stack.pop();
+                else if(c == ']' && !stack.empty() && stack.peek() == '[') stack.pop();
+                else if(c == '}' && !stack.empty() && stack.peek() == '{') stack.pop();
+                else return false;
             }
         }
-        return stack.size() == 0? true:false;
+        return stack.empty();
     }
 }
