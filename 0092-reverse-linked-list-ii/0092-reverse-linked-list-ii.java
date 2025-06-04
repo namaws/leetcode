71060661011269fc8fb6ac30fb4700a2209b311f
Mao <2maomao54@gmail.com>
2025-06-04 11:51:50 -0500

    Time: 0 ms (100%), Space: 41.7 MB (20.99%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,12 +10,12 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode preRange = new ListNode(0, head);
-        ListNode prev = preRange;
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
         ListNode curr = head;
+
         int cnt = 1;
         while(curr != null) {
-            //remove the connection for the first node (need to re-connect later)
             if(cnt == left) {
                 ListNode temp = curr.next;
                 prev.next = null;
@@ -23,7 +23,7 @@ class Solution {
                 prev = curr;
                 curr = temp;
             }
-            //reverse next pointer
+
             else if(cnt > left && cnt <= right) {
                 ListNode temp = curr.next;
                 curr.next = prev;
@@ -39,18 +39,17 @@ class Solution {
             cnt++;
         }
 
-        ListNode dummy = preRange;
+        //connect reverse with head and tail
+        ListNode preRange = dummy;
         while(preRange.next != null) {
             preRange = preRange.next;
         }
 
-        //prev is the first node (right) of the reverse list
         preRange.next = prev;
 
         while(preRange.next != null) {
             preRange = preRange.next;
         }
-        //curr is the first node after the reverse list
         preRange.next = curr;
         return dummy.next;
 
