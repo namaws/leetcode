38ff62254fc3ce7bf5d5c9b4028669a23dda5de7
Mao <2maomao54@gmail.com>
2025-06-04 10:07:31 -0500

    Time: 6 ms (7.58%), Space: 44.6 MB (96.97%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,22 +1,30 @@
 class MinStack {
     List<Integer> list;
     Stack<Integer> stack;
-    
+
     public MinStack() {
         list = new ArrayList();
         stack = new Stack();
     }
     
     public void push(int val) {
-        list.add(val);
-        if(stack.isEmpty() || val <= stack.peek()) {
+        if(stack.empty() || val <= stack.peek()) {
             stack.push(val);
         }
+        list.add(val);
     }
     
     public void pop() {
-        if(stack.peek().equals(list.get(list.size()-1))) {
-            stack.pop();
+        int val = list.get(list.size()-1);
+        /**
+        Integer Caching and == Comparison
+        In Java, when comparing two Integer objects using ==, you’re comparing object references, not values, unless they're in the cached range [-128 to 127].
+
+        list.get(...) and stack.peek() both return Integer objects.
+        So list.get(...) == stack.peek() might return false even if the values are the same if they are not the same object.
+ */
+        if(list.get(list.size()-1).equals(stack.peek())) {
+            System.out.println("pop: "+stack.pop());
         }
         list.remove(list.size()-1);
     }
