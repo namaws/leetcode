b8007041181b3bf6c6fc6ab4aceb3ec3903a23cd
Mao <2maomao54@gmail.com>
2025-10-03 10:22:14 -0500

    Time: 116 ms (76.24%), Space: 57.3 MB (93.13%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -14,11 +14,12 @@ class RandomizedSet:
     def remove(self, val: int) -> bool:
         if val in self.dict:
             last_element, idx = self.list[-1], self.dict[val]
-            self.list[idx], self.dict[last_element] = last_element, idx
+            self.dict[last_element], self.list[idx] = idx, last_element
             self.list.pop()
             del self.dict[val]
             return True
         return False
+
 
     def getRandom(self) -> int:
         return choice(self.list)
