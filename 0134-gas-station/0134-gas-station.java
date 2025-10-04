b029a8d77a744f3af1b563764ec1de4667378b9f
Mao <2maomao54@gmail.com>
2025-10-04 16:38:49 -0500

    Time: 3 ms (28.48%), Space: 55.6 MB (96.56%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,18 +1,14 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int start = 0;
-        int curr_gas = 0;
-        int total_diff = 0; // diff between gas and cost
-        
-        for(int i=0; i<cost.length; i++) {
-            total_diff += gas[i] - cost[i];
-            curr_gas += gas[i] - cost[i];
-            if(curr_gas < 0) {
-                curr_gas = 0;
+        int totalDiff = 0, start = 0, currGas = 0;
+        for(int i=0; i<gas.length; i++) {
+            totalDiff += gas[i]-cost[i];
+            currGas += gas[i]-cost[i];
+            if(currGas < 0) {
+                currGas = 0;
                 start = i+1;
-            } 
+            }
         }
-        
-        return total_diff >= 0 ? start:-1;
+        return totalDiff >= 0? start: -1;
     }
 }
